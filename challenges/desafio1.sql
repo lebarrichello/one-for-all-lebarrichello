8d67bf855d37ec5a3f2662a818bf03adfd6314bc
lebarrichello <leticia.barrichello@gmail.com>
2023-08-25 12:17:42 -0300

    Requisito 1 - Finalmente saiu - Normalizacao, criacao e insercao de dados na tabela

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -1,12 +1,11 @@
 DROP DATABASE IF EXISTS SpotifyClone;
-CREATE DATABASE SpotifyClone;
-
+CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
 CREATE TABLE SpotifyClone.plan (
-    plan_id INT AUTO_INCREMENT PRIMARY KEY,
-    plan_name VARCHAR(255),
-    plan_value DECIMAL(10, 2)
-) engine = InnoDB;
+    plan_id INT PRIMARY KEY AUTO_INCREMENT,
+    plan_name VARCHAR(255) NOT NULL,
+    plan_value DECIMAL(10 , 2 ) NOT NULL
+)  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.plan (plan_name, plan_value)
 VALUES
@@ -14,12 +13,12 @@ VALUES
     ('Familiar', 7.99),
     ('Universitário', 5.99),
     ('Pessoal', 6.99);
+
     
-    
-    CREATE TABLE SpotifyClone.artists (
-    artist_id INT AUTO_INCREMENT PRIMARY KEY,
-    artist_name VARCHAR(255)
-) engine = InnoDB;
+CREATE TABLE SpotifyClone.artists (
+    artist_id INT PRIMARY KEY AUTO_INCREMENT,
+    artist_name VARCHAR(255) NOT NULL
+)  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.artists (artist_name)
 VALUES
@@ -34,12 +33,13 @@ VALUES
 
 CREATE TABLE SpotifyClone.users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255),
-    user_age INT,
-    plan_signature_date DATE,
-    plan_id INT,
-    FOREIGN KEY (plan_id) REFERENCES plan(plan_id)
-) engine = InnoDB;
+    user_name VARCHAR(255) NOT NULL,
+    user_age INT NOT NULL,
+    plan_signature_date DATE NOT NULL,
+    plan_id INT NOT NULL,
+    FOREIGN KEY (plan_id)
+        REFERENCES plan (plan_id)
+)  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.users (user_name, user_age,plan_signature_date,plan_id)
 VALUES
@@ -53,16 +53,18 @@ VALUES
 	('Christopher Alexander',85,20190605,4),
 	('Judith Butler',45,20200513,4),
 	('Jorge Amado',58,20170217,4);
-    
+ 
 
 CREATE TABLE SpotifyClone.following_artists (
     following_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    artist_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
-    UNIQUE(user_id, artist_id)
-) engine = InnoDB;
+    user_id INT NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id),
+    FOREIGN KEY (artist_id)
+        REFERENCES artists (artist_id),
+    UNIQUE KEY (user_id , artist_id)
+)  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.following_artists (user_id,artist_id)
 VALUES
@@ -77,20 +79,20 @@ VALUES
     (5,6),
     (6,6),
     (6,1),
-    (7,1),
-    (8),
+    (7,6),
     (9,3),
     (10,2);
-    
+ 
 
 
-CREATE TABLE SpotifyClone.albums(
+CREATE TABLE SpotifyClone.albums (
     album_id INT AUTO_INCREMENT PRIMARY KEY,
-    album_name VARCHAR(255),
-    album_release_year INT,
-    artist_id INT,
-    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
-) engine = InnoDB;
+    album_name VARCHAR(255) NOT NULL,
+    album_release_year INT NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (artist_id)
+        REFERENCES artists (artist_id)
+)  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.albums(album_name, album_release_year,artist_id)
 VALUES
@@ -102,16 +104,17 @@ VALUES
     ('QVVJFA?',2003,4),
     ('Somewhere Far Beyond',2007,5),
     ('I Put A Spell On You',2012,6);
+ 
     
     
-    
-CREATE TABLE SpotifyClone.songs(
+CREATE TABLE SpotifyClone.songs (
     music_id INT AUTO_INCREMENT PRIMARY KEY,
-    music_name VARCHAR(255),
-    music_time INT,
-    album_id INT,
-    FOREIGN KEY (album_id) REFERENCES albums(album_id)
-) engine = InnoDB;
+    music_name VARCHAR(255) NOT NULL,
+    music_time INT NOT NULL,
+    album_id INT NOT NULL,
+    FOREIGN KEY (album_id)
+        REFERENCES albums (album_id)
+)  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.songs(music_name, music_time,album_id)
 VALUES
@@ -125,34 +128,37 @@ VALUES
     ('Samba em Paris',267,6),
     ('The Bards Song',244,7),
     ('Feeling Good',100,8);
-    
+  
     
     
 CREATE TABLE SpotifyClone.playback_history (
     history_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    music_id INT,
+    user_id INT NOT NULL,
+    music_id INT NOT NULL,
     reproduction_date DATETIME,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (music_id) REFERENCES songs(music_id),
-    UNIQUE(user_id, music_id)
-) engine = InnoDB;
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id),
+    FOREIGN KEY (music_id)
+        REFERENCES songs (music_id),
+    UNIQUE KEY (user_id , music_id)
+)  ENGINE=INNODB;
 
-INSERT INTO SpotifyClone.playback_history (user_id,reproduction_date,music_id)
+INSERT INTO SpotifyClone.playback_history (user_id, reproduction_date, music_id)
 VALUES
-	('1', '2022-02-28 10:45:55', '1'),
-	('1', '2020-05-02 05:30:35', '2'),
-	('1', '2020-03-06 11:22:33', '3'),
-	('2', '2022-08-05 08:05:17', '3'),
-	('2', '2020-01-02 07:40:33', '6'),
-	('3', '2020-11-13 16:55:13', '3'),
-	('3', '2020-12-05 18:38:30', '2'),
-	('4', '2021-08-15 17:10:10', '1'),
-	('5', '2022-01-09 01:44:33', '1'),
-	('5', '2020-08-06 15:23:43', '5'),
-	('6', '2017-01-24 00:31:17', '6'),
-	('6', '2017-10-12 12:35:20', '7'),
-	('7', '2011-12-15 22:30:49', '8'),
-	('8', '2012-03-17 14:56:41', '8'),
-	('9', '2022-02-24 21:14:22', '9'),
-	('10', '2015-12-13 08:30:22', '10');
+	(1, '2022-02-28 10:45:55', 1),
+	(1, '2020-05-02 05:30:35', 2),
+	(1, '2020-03-06 11:22:33', 3),
+	(2, '2022-08-05 08:05:17', 3),
+	(2, '2020-01-02 07:40:33', 6),
+	(3, '2020-11-13 16:55:13', 3),
+	(3, '2020-12-05 18:38:30', 2),
+	(4, '2021-08-15 17:10:10', 1),
+	(5, '2022-01-09 01:44:33', 1),
+	(5, '2020-08-06 15:23:43', 5),
+	(6, '2017-01-24 00:31:17', 6),
+	(6, '2017-10-12 12:35:20', 7),
+	(7, '2011-12-15 22:30:49', 8),
+	(8, '2012-03-17 14:56:41', 8),
+	(9, '2022-02-24 21:14:22', 9),
+	(10, '2015-12-13 08:30:22', 10);
+
