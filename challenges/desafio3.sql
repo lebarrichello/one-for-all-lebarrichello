d7b2ce2ad889ea000b8f1d29485d130df35af8f9
lebarrichello <leticia.barrichello@gmail.com>
2023-08-28 19:56:21 -0300

    Requisito 3 e 5

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,11 +1,10 @@
 SELECT
-    u.user_name AS pessoa_usuaria,
-    COUNT(DISTINCT ph.music_id) AS musicas_ouvidas,
-    ROUND(SUM(s.music_time) / 60, 2) AS total_minutos
-FROM SpotifyClone.users u
-LEFT JOIN SpotifyClone.playback_history ph ON u.user_id = ph.user_id
-LEFT JOIN SpotifyClone.songs s ON ph.music_id = s.music_id
-GROUP BY u.user_name
-ORDER BY u.user_name;
-
+    U.user_name AS pessoa_usuaria,
+    COUNT(LH.music_id) AS musicas_ouvidas,
+    ROUND(SUM(S.music_time / 60), 2) AS total_minutos
+FROM SpotifyClone.users AS U
+    JOIN SpotifyClone.playback_history as LH ON U.user_id = LH.user_id
+    JOIN SpotifyClone.songs as S ON S.music_id = LH.music_id
+GROUP BY pessoa_usuaria
+ORDER BY pessoa_usuaria;
 
--- challenges/desafio5.sql
+++ challenges/desafio5.sql
@@ -1,8 +1,10 @@
 SELECT
-    s.music_name AS cancao,
-    COUNT(ph.user_id) AS reproducoes
-FROM SpotifyClone.songs s
-LEFT JOIN SpotifyClone.playback_history ph ON s.music_id = ph.music_id
-GROUP BY s.music_name
-ORDER BY reproducoes DESC, cancao ASC
+    S.music_name AS cancao,
+    COUNT(LH.music_id) AS reproducoes
+FROM SpotifyClone.songs as S
+    JOIN SpotifyClone.playback_history as LH ON S.music_id = LH.music_id
+GROUP BY LH.music_id
+ORDER BY
+    reproducoes DESC,
+    cancao
 LIMIT 2;
